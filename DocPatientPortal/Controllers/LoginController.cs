77bb06a6e7124c216c7be0bfffdde6545e26a49e
python22-python <buddhaenlighten3@gmail.com>
2020-12-01 23:24:15 +0545

    initial

---
+++ DocPatientPortal/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocPatientPortal.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginCheck()
+        {
+            //checking the username and password
+            //write the code here and redirect to the home page if condition comes true.
+
+            //if (username == true && password == true)
+            //return Redirect("Home/Index")
+
+
+            return Redirect("/Home/Index");
+        }
+    }
+}
