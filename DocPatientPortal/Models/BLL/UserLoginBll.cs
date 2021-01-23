30d55e9edb028cdc2d6b47a72fc3ddb7bb824950
python22-python <buddhaenlighten3@gmail.com>
2021-01-23 11:39:40 +0545

    magh 10

---
+++ DocPatientPortal/Models/BLL/UserLoginBll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocPatientPortal.Models.BLL
+{
+    class UserLoginBll
+    {
+
+        public string username { get; set; }
+        public string password { get; set; }
+        string role { get; set; }
+        public bool _isAdmin {
+            get
+            {
+                return false;
+            }
+            set
+            {
+                if (role =="admin")
+                {
+                    value = true;
+                }
+            }
+        }
+    }
+}
---
+++ DocPatientPortal/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocPatientPortal.Models
+{
+    [Table("user")]
+    public class User
+    {
+        [Key]
+        public int pid { get; set; }
+        public string pname { get; set; }
+        public string paddress { get; set; }
+        public string pblood { get; set; }
+    }
+}
